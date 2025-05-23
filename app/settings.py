101b8986e9d39d2aa5e94185afd0fad36863e0b2
Sowndarya Jupalli <sowndarya@asterteq.com>
2025-05-23 15:52:56 +0530

    Fix flake8 line length warnings

--- app/settings.py
+++ app/settings.py
@@ -20,8 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-unzv9+hl7ttygbvovjlr$duen8uh&6(0j(q)9_hudoh)*=as7b'
-
+#SECRET_KEY = 'django-insecure-unzv9+hl7ttygbvovjlr$duen8uh&6(0j(q)9_hudoh)*=as7b'
+SECRET_KEY = (
+    'django-insecure-unzv9+hl7ttygbvovjlr$duen8uh&6(0j(q)9_hudoh)*='
+    'as7b'
+)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -84,7 +87,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
+'''AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
@@ -97,7 +100,34 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
+]'''
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'UserAttributeSimilarityValidator'
+        ),
+    },
+    {
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'MinimumLengthValidator'
+        ),
+    },
+    {
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'CommonPasswordValidator'
+        ),
+    },
+    {
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'NumericPasswordValidator'
+        ),
+    },
 ]
+
 
 
 # Internationalization
