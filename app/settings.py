073b880290bcbdfac405988896a34a099774f7f7
Sowndarya Jupalli <sowndarya@asterteq.com>
2025-05-23 16:00:30 +0530

    Fix flake8 line length warnings

--- app/settings.py
+++ app/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = 'django-insecure-unzv9+hl7ttygbvovjlr$duen8uh&6(0j(q)9_hudoh)*=as7b'
 SECRET_KEY = (
-    'django-insecure-unzv9+hl7ttygbvovjlr$duen8uh&6(0j(q)9_hudoh)*='
-    'as7b'
+    'django-insecure-unzv9+hl7ttygbvovjlr$duen8uh&6(0j(q)9_hudoh)*=as7b'
 )
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -84,10 +83,10 @@ DATABASES = {
 }
 
 
-# Password validation
-# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
+'''Password validation
+ https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
-'''AUTH_PASSWORD_VALIDATORS = [
+AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
@@ -103,33 +102,18 @@ DATABASES = {
 ]'''
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': (
-            'django.contrib.auth.password_validation.'
-            'UserAttributeSimilarityValidator'
-        ),
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': (
-            'django.contrib.auth.password_validation.'
-            'MinimumLengthValidator'
-        ),
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME': (
-            'django.contrib.auth.password_validation.'
-            'CommonPasswordValidator'
-        ),
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        'NAME': (
-            'django.contrib.auth.password_validation.'
-            'NumericPasswordValidator'
-        ),
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
